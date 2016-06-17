33eb71c81eb7d44db023bfac9f171ddb55af61fc
DinhThienLapDia <sworduser.no2@gmail.com>
2016-06-17 11:46:57 +0700

    first commit

---
+++ testfull.py
@@ -0,0 +1,90 @@
+'''
+Created on Jun 11, 2016
+
+@author: admin
+'''
+from flask import Flask, request, Response, jsonify, json
+import pymongo
+from bson import ObjectId
+import datetime
+
+app = Flask(__name__)
+client = pymongo.MongoClient()
+db = client.testdatabase
+
+@app.route('/newticket', methods=["POST"])
+def newticket():
+    try:
+        data = request.form["user"]
+        print data
+        jsondata = json.loads(data)
+        print jsondata
+        #user = jsondata["user"]
+        #text = jsondata["text"]
+        time_created = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        replies = []
+        #file = request.file("image")
+        status = 0
+        jsondata.update({"time_created":time_created,"status":status, "replies":replies})
+        db.supportticket.insert(jsondata)
+        return jsonify({"status":0})
+    except Exception as e:
+        return jsonify({"error":str(e)})
+
+@app.route('/listticket', methods=["POST"])
+def listticket():
+    data = request.form["user"]
+    print data
+    jsondata = json.loads(data)
+    print jsondata
+    ticketlist = []
+    cursor = db.supportticket.find(jsondata)
+    for document in cursor:
+        str_id = str(document["_id"])
+        document["_id"]= str_id
+        ticketlist.append(document)
+    print ticketlist
+    return jsonify({"tickets":ticketlist})
+@app.route('/updateticket', methods=["POST"])
+def updateticket():
+    data = request.form["ticket"]
+    jsondata = json.loads(data)
+    print jsondata
+    ticketid = ObjectId(jsondata["_id"])
+    print ticketid
+    ticket = db.supportticket.find_one({"_id":ticketid})
+    print ticket
+    replies_current = []
+    if ticket["replies"] is not None:
+        replies_current = ticket["replies"]
+    else:
+        replies_current = []
+    print replies_current
+    new_reply = json.loads(request.form["reply"])
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    new_reply.update({"time_replies":current_time})
+    print new_reply
+    replies_current.append(new_reply)
+    print replies_current
+    result = db.supportticket.update({"_id":ticketid}, {"$set":{"replies":replies_current}})
+    if result:
+        return jsonify({"return_code":0})
+    else:
+        return jsonify({"return_code":1})
+@app.route('/closeticket', methods =['POST'])
+def closeticket():
+    data = request.form["ticket"]
+    jsondata = json.loads(data)
+    print jsondata
+    ticketid = ObjectId(jsondata["_id"])
+    print ticketid
+    ticket = db.supportticket.find_one({"_id":ticketid})
+    print ticket
+    result = db.supportticket.update({"_id":ticketid}, {"$set":{"status":1}})
+    if result:
+        return jsonify({"return_code":0})
+    else:
+        return jsonify({"return_code":1})
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1",port=8287,debug=True, threaded=True)
---
+++ util.py
@@ -0,0 +1,62 @@
+'''
+Created on Jun 13, 2016
+
+@author: admin
+'''
+import functools
+from flask import make_response
+from json import JSONEncoder, dumps
+from bson import ObjectId
+from datetime import date, datetime, time
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            # return obj.strftime("%Y-%m-%d %H:%M:%S")
+            # return obj.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            return obj.strftime("%Y-%m-%d")
+            # return obj.timestamp()
+        elif isinstance(obj, date):
+            return obj.strftime("%Y-%m-%d")
+        elif isinstance(obj, ObjectId):
+            return str(obj)
+        else:
+            return JSONEncoder.default(self, obj)
+
+
+def json2(f):
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        rv = f(*args, **kwargs)
+
+        status = None
+        headers = []
+        if isinstance(rv, tuple):
+            rv, status, headers = rv + (None,) * (3 - len(rv))
+        if isinstance(status, (dict, list)):
+            headers, status = status, None
+
+        # if not isinstance(rv, dict):
+        #     rv = rv.export_data()
+        status_code = 200
+
+        if status:
+            status_code = status
+        rv = make_response(dumps(rv, cls=CustomJSONEncoder), status_code, {"Content-Type": "application/json"})
+        print(rv.data)
+        if headers is not None:
+            rv.headers.extend(headers)
+        rv.headers.extend(make_cors_headers())
+
+        return rv
+    return wrapped
+
+
+def make_cors_headers():
+    cors_headers = {
+        "Access-Control-Allow-Headers": "Origin, Accept, Content-Type, Authorization",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS, HEAD",
+        "Access-Control-Allow-Origin": "*"
+    }
+    return cors_headers
